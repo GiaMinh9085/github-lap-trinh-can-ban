925173d3db6d20bf6b70e52a430a5be89c3012ad
GiaMinh9085 <107554539+GiaMinh9085@users.noreply.github.com>
2022-06-24 21:20:34 +0700

    buoi 5

---
+++ SuDungMang/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SuDungMang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            KhaiBaoMang();
+            XuatMangRaManHinh();
+            XuatSoChanTrongMang();
+        }
+
+        private static void XuatSoChanTrongMang()
+        {
+            int[] a = { 2342, 23, 42342, 34, 234, 23 };
+            for (int i = 0; i < a.Length; i++)
+            {
+
+            }
+        }
+
+        private static void XuatMangRaManHinh()
+        {
+            int[] a = { 23, 423, 2, 2, 3, 23, 23, 421, 3413, 42 };
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+
+        private static void KhaiBaoMang()
+        {
+            int[] a = { 23, 23, 2, 2, 32, 2, 32, 12};
+            int[] b= new int[4];
+        }
+    }
+}
